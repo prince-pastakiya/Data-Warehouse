8b4f0a1506658261d42c37ab8c0383d8745c733c
Prince H. Pastakiya <93633708+prince-pastakiya@users.noreply.github.com>
2025-07-29 13:24:02 -0500

    Update DDL_Bronze.sql

--- scripts/Bronze/DDL_Bronze.sql
+++ scripts/Bronze/DDL_Bronze.sql
@@ -3,9 +3,9 @@
 DDL Script: Create Bronze Tables
 ===============================================================================
 Script Purpose:
-    This script creates tables in the 'bronze' schema, dropping existing tables 
-    if they already exist.
-	  Run this script to re-define the DDL structure of 'bronze' Tables
+    This script creates tables in the 'Bronze' schema, dropping existing tables. 
+    If they already exist.
+	  Run this script to redefine the DDL structure of 'Bronze' Tables
 ===============================================================================
 */
 
